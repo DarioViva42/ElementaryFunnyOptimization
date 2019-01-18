819f081f8fbd167cc684d4b66d7056c4de176282
DarioViva42 <darioviva@gmail.com>
2019-01-18 22:30:35 +0100

    neue Aenderung

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Jetzt ist es anders.");
-        System.out.println("nochmals anders");
+        System.out.println("nochmals anders.");
     }
 }
