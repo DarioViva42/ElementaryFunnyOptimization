aab0bc53b60dd96fb5a7c8f4422ef97ec0ba2604
DarioViva42 <darioviva@gmail.com>
2019-02-03 22:08:12 +0100

    platzierung von text verändert

--- src/com/efo/game/Main.java
+++ src/com/efo/game/Main.java
@@ -110,17 +110,15 @@ public class Main extends AbstractGame {
 
     //i += .01;
 
-
-    if(ge.getInput().getMouseX() > 180 && ge.getInput().getMouseX() < 305 && ge.getInput().getMouseY() > 30 && ge.getInput().getMouseY() < 70 || ussEnterprise.getPos().getX() > 180 && ussEnterprise.getPos().getX() < 315 && ussEnterprise.getPos().getY() > 30 && ussEnterprise.getPos().getY() < 70) {
-      r.drawImage(hover, 250,50, 0);
-      r.drawText("Halloo", 220, 45, 0xffff00ff);
-      if(ge.getInput().isButton(1) || ge.getInput().isKey(KeyEvent.VK_ENTER)) {
-        r.drawImage(clicked, 250,50,0);
-        r.drawText("Halloo", 220, 45, 0xffff00ff);
-      }
+    if(ge.getInput().isButton(1) || ge.getInput().isKey(KeyEvent.VK_ENTER)) {
+      r.drawImage(clicked, 250,50,0);
+      r.drawText("pressed", 195, 38, 0xffff00ff);
+    }else if(ge.getInput().getMouseX() > 180 && ge.getInput().getMouseX() < 305 && ge.getInput().getMouseY() > 30 && ge.getInput().getMouseY() < 70 || ussEnterprise.getPos().getX() > 180 && ussEnterprise.getPos().getX() < 315 && ussEnterprise.getPos().getY() > 30 && ussEnterprise.getPos().getY() < 70) {
+      r.drawImage(hover, 250, 50, 0);
+      r.drawText("Hover, ob du's glaubst oder", 195, 38, 0xffff00ff);
     } else {
-      r.drawImage(noHover, 250, 50, 0);
-        r.drawText("Halloo", 220, 45, 0xffff00ff);
+        r.drawImage(noHover, 250, 50, 0);
+        r.drawText("nicht auf Knopf", 195, 38, 0xffff00ff);
     }
 
 
